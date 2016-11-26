9b4a516a961ed6f17a8bcd798a481d14c8b26a35
Daniel Kamil Kozar <dkk089@gmail.com>
2016-11-26 23:03:05 +0100

    add a function for figuring out the name for UTF-8 used in the Java implementation

--- src/MainMidlet.java
+++ src/MainMidlet.java
@@ -1,5 +1,7 @@
 
+import java.io.UnsupportedEncodingException;
 import javax.microedition.lcdui.Alert;
+import javax.microedition.lcdui.AlertType;
 import javax.microedition.lcdui.Display;
 import javax.microedition.lcdui.Displayable;
 import javax.microedition.midlet.*;
@@ -18,7 +20,9 @@ public class MainMidlet extends MIDlet {
   }
 
   public void startApp() {
-    disp().setCurrent(mMainForm);
+    if (initializeUTF8()) {
+      disp().setCurrent(mMainForm);
+    }
   }
 
   public void pauseApp() {
@@ -41,7 +45,7 @@ public class MainMidlet extends MIDlet {
   }
 
   public void goBack() {
-    Displayable current = Display.getDisplay(this).getCurrent();
+    Displayable current = disp().getCurrent();
     if (current == mAddStopsForm) {
       disp().setCurrent(mMainForm);
     }
@@ -54,5 +58,34 @@ public class MainMidlet extends MIDlet {
 
   private Display disp() {
     return Display.getDisplay(this);
+  }
+
+  public static String getUTF8Name() {
+    return NAME_FOR_UTF8;
+  }
+  private static String NAME_FOR_UTF8;
+
+  private boolean initializeUTF8() {
+    String xyz = "foobar";
+    try {
+      // some J2ME implementations in real phones use UTF8 as the name for the encoding. check which
+      // one is supported and then use it throughout the application. bail if none is, since it
+      // won't be possible to send properly encoded data then.
+      NAME_FOR_UTF8 = "UTF-8";
+      xyz.getBytes(NAME_FOR_UTF8);
+    } catch (UnsupportedEncodingException ex1) {
+      try {
+        // pretty please...
+        NAME_FOR_UTF8 = "UTF8";
+        xyz.getBytes(NAME_FOR_UTF8);
+      } catch (UnsupportedEncodingException ex2) {
+        // nope.
+        Alert utf8NotSupported = new Alert("Błąd krytyczny", "Implementacja nie wspiera UTF-8. "
+                + "Aplikacja nie będzie działać poprawnie", null, AlertType.ERROR);
+        disp().setCurrent(utf8NotSupported, mMainForm);
+        return false;
+      }
+    }
+    return true;
   }
 }
