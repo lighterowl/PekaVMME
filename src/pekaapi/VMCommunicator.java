10e5ee2173fb8086a6076d6d99c78d37cda2770d
Daniel Kamil Kozar <dkk089@gmail.com>
2016-11-27 01:00:00 +0100

    add code for actually reading a JSON from the socket

--- src/pekaapi/VMCommunicator.java
+++ src/pekaapi/VMCommunicator.java
@@ -1,6 +1,7 @@
 package pekaapi;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.OutputStream;
 import java.io.UnsupportedEncodingException;
 import java.util.Vector;
@@ -8,6 +9,7 @@ import javax.microedition.io.Connector;
 import javax.microedition.io.HttpConnection;
 import midlet.MainMidlet;
 import org.json.me.JSONException;
+import org.json.me.JSONObject;
 
 public class VMCommunicator {
 
@@ -77,5 +79,71 @@ public class VMCommunicator {
       // this should never happen, since we check for this at app initialization time and explicitly
       // forbid the application from running.
     }
+  }
+
+  private interface ResultParser {
+
+    void parse(JSONObject o) throws JSONException;
+  }
+
+  private static void readVirtualMonitorData(ResultReceiver rcv, ResultParser p) {
+    HttpConnection conn = null;
+    OutputStream out = null;
+    InputStream in = null;
+    IOException ioEx = null;
+    JSONException jsonEx = null;
+    try {
+      conn = createConnection();
+      out = conn.openOutputStream();
+      addMethodParams(conn.openOutputStream(), "foo", "bar");
+      in = conn.openInputStream();
+      p.parse(getJSONFromInputStream(in));
+    } catch (IOException ex) {
+      ioEx = ex;
+    } catch (JSONException ex) {
+      jsonEx = ex;
+    } finally {
+      try {
+        // no option but to ignore these.
+        if (out != null) {
+          out.close();
+        }
+        if (in != null) {
+          in.close();
+        }
+        if (conn != null) {
+          conn.close();
+        }
+      } catch (IOException e) {
+      }
+    }
+  }
+
+  private static JSONObject getJSONFromInputStream(InputStream in)
+          throws IOException, JSONException {
+    // okay, bear with me, but this is why this is needed :
+    // * we can't use StringBuffer, since its interface is not compatible with a UTF-8 bytestream :
+    // it expects to be passed chars or strings. neither works here : a char is a 16-bit UTF-16
+    // value, and we can't construct Strings from every byte we read since it might not represent a
+    // whole UTF-8 character.
+    // * we could theoretically use Vector, but it requires a *single* Object on input. not only
+    // would we have to perform boxing, but also call read() with essentially a 1-byte buffer, which
+    // is an atrocity.
+    // so, the only viable solution is to slurp the whole bytestream into a manually-resized array
+    // and then interpret its contents as UTF-8. honestly, I've never expected to do such relatively
+    // low-level stuff in Java.
+
+    byte[] buf = new byte[4096]; // hopefully enough for one go...
+    int readBytes;
+    int offset = 0;
+    while ((readBytes = in.read(buf, offset, buf.length - offset)) != -1) {
+      offset += readBytes;
+      if (offset == buf.length) {
+        byte[] newbuf = new byte[buf.length * 2];
+        System.arraycopy(buf, 0, newbuf, 0, buf.length);
+        buf = newbuf;
+      }
+    }
+    return new JSONObject(new String(buf, 0, offset, MainMidlet.getUTF8Name()));
   }
 }
