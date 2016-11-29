976ad81cbe3cdf1e35857e64d17979604eadc55b
Daniel Kamil Kozar <dkk089@gmail.com>
2016-11-29 23:41:22 +0100

    change some labels

--- src/midlet/MainForm.java
+++ src/midlet/MainForm.java
@@ -1,6 +1,5 @@
 package midlet;
 
-
 import midlet.MainMidlet;
 import javax.microedition.lcdui.Command;
 import javax.microedition.lcdui.CommandListener;
@@ -10,11 +9,11 @@ import javax.microedition.lcdui.Form;
 public class MainForm extends Form {
 
   private static final Command EXIT_COMMAND = new Command("Wyjście", Command.EXIT, 1);
-  private static final Command ADD_STOPS_COMMAND = new Command("Dodaj przystanki", Command.SCREEN, 1);
+  private static final Command ADD_STOPS_COMMAND = new Command("Dodaj", Command.SCREEN, 1);
   private final MainMidlet mParent;
 
   public MainForm(MainMidlet parent) {
-    super("PEKA Virtual Monitor");
+    super("Przystanki");
     mParent = parent;
     addCommand(EXIT_COMMAND);
     addCommand(ADD_STOPS_COMMAND);
