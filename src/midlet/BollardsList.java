05ca75c198ae0324e042ce37bcbab67d0503b111
Daniel Kamil Kozar <dkk089@gmail.com>
2016-12-02 00:12:47 +0100

    don't create a separate form for bollard info : stuff information into the list by using linebreaks

--- src/midlet/BollardsList.java
+++ src/midlet/BollardsList.java
@@ -6,36 +6,47 @@ import javax.microedition.lcdui.Command;
 import javax.microedition.lcdui.CommandListener;
 import javax.microedition.lcdui.Displayable;
 import javax.microedition.lcdui.List;
-import pekaapi.Bollard;
+import pekaapi.BollardWithDirections;
+import pekaapi.LineDirection;
 
 public class BollardsList extends List {
 
   private final MainMidlet mMidlet;
   private final Vector mBollards;
-  private static final Command ADD_BOLLARDS = new Command("Dodaj", Command.ITEM, 1);
-  private static final Command BOLLARD_INFO = new Command("Szczegóły", Command.ITEM, 2);
+  private static final Command ADD_BOLLARDS = new Command("Zaznacz", Command.ITEM, 1);
   private static final Command BACK_COMMAND = new Command("Powrót", Command.BACK, 1);
 
-  public BollardsList(MainMidlet midlet, Vector bollards) {
+  public BollardsList(MainMidlet midlet, Vector bollardsWithDirections) {
     super("Lista słupków", Choice.MULTIPLE);
     mMidlet = midlet;
-    mBollards = bollards;
+    mBollards = bollardsWithDirections;
     for (int i = 0; i < mBollards.size(); ++i) {
-      append(((Bollard) mBollards.elementAt(i)).getTag(), null);
+      BollardWithDirections b = (BollardWithDirections) mBollards.elementAt(i);
+      append(b.getTag() + '\n' + getBollardSummary(b), null);
     }
     addCommand(ADD_BOLLARDS);
-    addCommand(BOLLARD_INFO);
     addCommand(BACK_COMMAND);
     setCommandListener(new CommandListener() {
       public void commandAction(Command c, Displayable d) {
         if (c == ADD_BOLLARDS) {
           // notify the midlet about the chosen bollards
-        } else if (c == BOLLARD_INFO) {
-          // display a form with bollard details
         } else if (c == BACK_COMMAND) {
           mMidlet.goBack();
         }
       }
     });
+  }
+
+  private String getBollardSummary(BollardWithDirections bollard) {
+    Vector directions = bollard.getDirections();
+    StringBuffer buf = new StringBuffer();
+    for (int i = 0; i < directions.size(); ++i) {
+      LineDirection d = (LineDirection) directions.elementAt(i);
+      buf.append(d.getLine()).append(" => ").append(d.getDestination());
+      if (i != directions.size() - 1) {
+        buf.append('\n');
+      }
+    }
+    return buf.toString();
   }
 }
