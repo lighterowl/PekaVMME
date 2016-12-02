ad9be6f9947fb0e9356c7ebe38fa98a2c33b715c
Daniel Kamil Kozar <dkk089@gmail.com>
2016-12-02 01:12:56 +0100

    add string serialization routines

--- src/util/Serialization.java
+++ src/util/Serialization.java
@@ -1,5 +1,7 @@
 package util;
 
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 
 public class Serialization {
@@ -29,4 +31,29 @@ public class Serialization {
     return NAME_FOR_UTF8;
   }
   private static String NAME_FOR_UTF8;
+
+  public static byte[] toByteArray(String s) {
+    byte[] encodedString = s.getBytes();
+    byte[] encodedLength = {
+      (byte) (encodedString.length >>> 24),
+      (byte) (encodedString.length >>> 16),
+      (byte) (encodedString.length >>> 8),
+      (byte) encodedString.length};
+    byte[] serialized = new byte[4 + encodedString.length];
+    System.arraycopy(encodedLength, 0, serialized, 0, 4);
+    System.arraycopy(encodedString, 0, serialized, 4, encodedString.length);
+    return serialized;
+  }
+
+  public static String fromByteArray(ByteArrayInputStream stream) throws IOException {
+    byte[] encodedLength = new byte[4];
+    stream.read(encodedLength);
+    int length = (encodedLength[0] << 24)
+            | (encodedLength[1] << 16)
+            | (encodedLength[2] << 8)
+            | encodedLength[3];
+    byte[] encodedString = new byte[length];
+    stream.read(encodedString);
+    return new String(encodedString);
+  }
 }
