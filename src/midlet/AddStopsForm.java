8e38398821b892396c48f37e18afc9d7263b9a0c
Daniel Kamil Kozar <dkk089@gmail.com>
2016-12-16 23:36:21 +0100

    don't store needless members in AddStopsForm

--- src/midlet/AddStopsForm.java
+++ src/midlet/AddStopsForm.java
@@ -11,17 +11,14 @@ import javax.microedition.lcdui.Form;
 
 public class AddStopsForm extends Form {
 
-  private final MainMidlet mParent;
-  private final TextField mPatternInput;
   private static final Command BACK_COMMAND = new Command("Powrót", Command.BACK, 1);
   private static final Command SEARCH_COMMAND = new Command("Szukaj", Command.SCREEN, 1);
 
-  public AddStopsForm(MainMidlet parent) {
+  public AddStopsForm(final MainMidlet parent) {
     super("Dodaj przystanek");
-    mParent = parent;
-    mPatternInput = new TextField("Nazwa", "", 50, TextField.ANY);
-    append(mPatternInput);
-    ChoiceGroup choices = new ChoiceGroup("Szukaj wśród...",
+    final TextField patternInput = new TextField("Nazwa", "", 50, TextField.ANY);
+    append(patternInput);
+    final ChoiceGroup choices = new ChoiceGroup("Szukaj wśród...",
             ChoiceGroup.EXCLUSIVE,
             new String[]{"przystanków", "ulic", "linii"}, null);
     append(choices);
@@ -30,15 +27,26 @@ public class AddStopsForm extends Form {
     setCommandListener(new CommandListener() {
       public void commandAction(Command c, Displayable d) {
         if (c == BACK_COMMAND) {
-          mParent.goBack();
+          parent.goBack();
         } else if (c == SEARCH_COMMAND) {
-          String pattern = mPatternInput.getString();
+          String pattern = patternInput.getString();
           if (pattern.length() == 0) {
-            mParent.displayAlert(new Alert("Błąd", "Wpisana nazwa jest pusta", null,
+            parent.displayAlert(new Alert("Błąd", "Wpisana nazwa jest pusta", null,
                     AlertType.ERROR));
             return;
           }
-          mParent.displayStopPoints(pattern);
+          switch (choices.getSelectedIndex()) {
+            case 0:
+              parent.displayStopPoints(pattern);
+              break;
+            case 1:
+              // displayStopPointsByStreet
+              break;
+            case 2:
+              // displayStopPointsByLine
+              break;
+          }
+
         }
       }
     });
